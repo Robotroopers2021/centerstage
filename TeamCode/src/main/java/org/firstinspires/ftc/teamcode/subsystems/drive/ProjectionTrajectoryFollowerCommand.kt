c3a5ef0e5fd1f61ad09df527e69d47247b297894
aneesh <aneeshjosyula@gmail.com>
2023-09-24 16:39:02 -0400

    made follower command good

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/drive/ProjectionTrajectoryFollowerCommand.kt
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.subsystems.drive
+
+import com.acmerobotics.dashboard.FtcDashboard
+import com.acmerobotics.dashboard.canvas.Canvas
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket
+import com.acmerobotics.roadrunner.Action
+import com.arcrobotics.ftclib.command.CommandBase
+
+class ProjectionTrajectoryFollowerCommand(private val drive: MecanumDriveSubsystem, private val trajectory: Action) :
+    CommandBase() {
+    val dash = FtcDashboard.getInstance()
+    val c = Canvas()
+    var b = true
+    init {
+        addRequirements(drive)
+    }
+
+    override fun initialize() {
+        trajectory.preview(c)
+    }
+
+    override fun execute() {
+        val p = TelemetryPacket()
+        p.fieldOverlay().operations.addAll(c.operations)
+
+        b = trajectory.run(p)
+
+        dash.sendTelemetryPacket(p)
+    }
+
+    override fun isFinished(): Boolean {
+        return !b || Thread.currentThread().isInterrupted
+    }
+}
