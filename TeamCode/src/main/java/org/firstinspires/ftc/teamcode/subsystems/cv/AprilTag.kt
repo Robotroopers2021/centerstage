0e5e2c364d0656752eed26c09ee7ba3a64ca084d
aneesh <aneeshjosyula@gmail.com>
2023-09-30 13:47:57 -0400

    i hate private variables

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/cv/AprilTag.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/cv/AprilTag.kt
@@ -1,33 +1,31 @@
 package org.firstinspires.ftc.teamcode.subsystems.cv
 
-import android.util.Log
 import android.util.Size
-import com.acmerobotics.roadrunner.Twist2d
 import com.acmerobotics.roadrunner.Vector2d
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
-import com.qualcomm.robotcore.eventloop.opmode.OpMode
-import com.qualcomm.robotcore.eventloop.opmode.OpModeManager
 import com.qualcomm.robotcore.hardware.HardwareMap
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.launch
-import org.firstinspires.ftc.robotcontroller.internal.FtcOpModeRegister
-import org.firstinspires.ftc.robotcontroller.internal.FtcRobotControllerActivity
-import org.firstinspires.ftc.robotcore.external.hardware.camera.Camera
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit
+import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration
 import org.firstinspires.ftc.vision.VisionPortal
+import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessorImpl
+import org.opencv.core.Mat
+import org.openftc.apriltag.AprilTagDetectorJNI
 
 @OptIn(DelicateCoroutinesApi::class)
 class AprilTag(hardwareMap: HardwareMap, opMode: LinearOpMode) {
     var aprilTag: AprilTagProcessor = AprilTagProcessor.Builder()
-        /*.setDrawAxes(true)
-        .setDrawCubeProjection(true)
-        .setDrawTagOutline(true)*/
         .setNumThreads(6)
         .setLensIntrinsics(876.376640148741, 879.046105394020, 633.420091455433, 408.814246529796)
         .setOutputUnits(DistanceUnit.INCH, AngleUnit.RADIANS)
